ed0c91691d5b7c7fa90a4ef25e847cdde7297ec3
rayhan <prog.rayhan1@gmail.com>
2022-04-13 14:48:16 +0600

    homepage upgrade

--- pages/Posts/HomePage.js
+++ pages/Posts/HomePage.js
@@ -4,7 +4,7 @@ import React from 'react'
 function HomePage() {
   return (
     <div className='flex justify-center text-center flex-col'>
-      <h1 className='mt-20 text-3xl'>Welcome to Next JS</h1>
+      <h1 className='mt-20 text-3xl'>This is HomePage</h1>
       <br/>
       <Link href="/" passHref>
       <button>Go back to home</button>
